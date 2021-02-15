ec77b358a950ee6f1ab3c7fcbbe5eee3ee4b355d
itsyaboilulu <lwsnsh@googlemail.com>
2021-02-15 12:47:00 +0000

    fix view hight on chrom mobile

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
-        <link rel="stylesheet" href="resources/css/main.min.css?v=1.1" />
+        <link rel="stylesheet" href="resources/css/main.min.css?v=1.2" />
         @yield('head')
         <title>Uno</title>
     </head>
