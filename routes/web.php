2e13fd9540b9d8ad031da67eb32ca0db6c5f1e2e
itsyaboilulu <lwsnsh@googlemail.com>
2021-02-20 02:53:24 +0000

    remove convert code

--- routes/web.php
+++ routes/web.php
@@ -40,7 +40,6 @@ Auth::routes();
 
 Route::get('/test',         function()
 {
-    return (new testController())->test();
     if (app()->environment() == 'local'){
         return (new testController())->test();
     }
